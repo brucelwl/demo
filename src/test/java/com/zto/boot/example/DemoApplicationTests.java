94cf3ca42864967629ee4255cb58d7de23ee0219
lwl <liwenlong@zto.cn>
2019-07-15 13:08:13 +0800

    测试ActiveProfiles

--- src/test/java/com/zto/boot/example/DemoApplicationTests.java
+++ src/test/java/com/zto/boot/example/DemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.zto.boot.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
+@ActiveProfiles("fat")
 public class DemoApplicationTests {
+
+    @Autowired
+    private Environment environment;
 
     @Test
     public void contextLoads() {
+
+        String property = environment.getProperty("sharding.basePackage");
+
+        System.out.println(property);
+
     }
 
 }
