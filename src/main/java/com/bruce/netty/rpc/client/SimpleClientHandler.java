f33ccc01aa1ad4de8d2d4b57980c3803c99b651a
bruce <841326226@qq.com>
2020-12-12 14:14:45 +0800

    EncoderException hidden when sending

--- src/main/java/com/bruce/netty/rpc/client/SimpleClientHandler.java
+++ src/main/java/com/bruce/netty/rpc/client/SimpleClientHandler.java
@@ -16,4 +16,9 @@ public class SimpleClientHandler extends ChannelInboundHandlerAdapter {
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         super.userEventTriggered(ctx, evt);
     }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
 }
