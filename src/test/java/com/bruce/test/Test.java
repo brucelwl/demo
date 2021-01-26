cd56cbcd6a0b25b08701bdab21aa110ed8639c56
bruce <841326226@qq.com>
2021-01-26 20:21:30 +0800

    ..

--- src/test/java/com/bruce/test/Test.java
+++ src/test/java/com/bruce/test/Test.java
@@ -1,6 +1,7 @@
 package com.bruce.test;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.StampedLock;
 
 /**
  * Created by bruce on 2021/1/11 9:47
@@ -9,14 +10,30 @@ public class Test {
 
     public static void main(String[] args) throws InterruptedException {
 
-        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>(10);
-        queue.offer("aaa");
-        queue.offer("bbb");
-        queue.offer("ccc");
+        // LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>(10);
+        // queue.offer("aaa");
+        // queue.offer("bbb");
+        // queue.offer("ccc");
+        //
+        // for (String s : queue) {
+        //     System.out.println(queue.poll());
+        // }
 
-        for (String s : queue) {
-            System.out.println(queue.poll());
+
+        StampedLock stampedLock = new StampedLock();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(stampedLock.tryOptimisticRead());
+                }
+            }).start();
         }
+
+
+        stampedLock.tryWriteLock();
+
 
     }
 }
